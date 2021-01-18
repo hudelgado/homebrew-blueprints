d69513d9390a399777dcfb8257915ea61d61c624
Hugo Delgado <2730695+hudelgado@users.noreply.github.com>
2021-01-18 00:51:07 +0000

    telepresence: add 0.109-alpha bottle.

--- Formula/telepresence.rb
+++ Formula/telepresence.rb
@@ -7,6 +7,12 @@ class Telepresence < Formula
   sha256 "66aa235c33852c635079ccfbfd6c1001d28b05b3bccd6af11af94569c0f6039a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/hudelgado/homebrew-blueprints/releases/download/telepresence-0.109-alpha"
+    cellar :any_skip_relocation
+    sha256 "8474ab3b997d8fe96ff0febcbb7b89133286d375a2106da89b94b2a140783cdf" => :x86_64_linux
+  end
+
   depends_on "python"
   depends_on "sshfs"
   depends_on "torsocks"
